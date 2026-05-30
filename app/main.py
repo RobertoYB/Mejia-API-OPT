57378689d80b219265d0af7519524537d6532cde
Scorpion-CC <carloscantom@gmail.com>
2026-05-29 19:27:46 -0600

    Añadir CORS a la API
    Se añadió CORS a la API para que se puedan hacer llamadas desde el frontend

--- app/main.py
+++ app/main.py
@@ -3,8 +3,24 @@ from app.API.router_odoo import router as api_router_odoo
 from app.API.router_prestashop import router as api_router_prestashop
 from app.API.router_bridge import router as api_router_bridge
 from app.API.router_wordpress import router as api_router_wordpress
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
